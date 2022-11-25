5d3d4d1ee59bda77446641e986bba7403166404e
milahristova <hristova.mila@abv.bg>
2022-11-25 15:07:39 +0200

    Adds delete method

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -58,6 +58,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.removeResource,
     };
   },
   methods: {
@@ -75,6 +76,10 @@ export default {
       this.storedResources.unshift(newResource);
       this.selectedTab = 'stored-resources';
     },
+  },
+  removeResource(resId) {
+    const resIndex = this.storedResources.findIndex(res => res.id = resId);
+    this.storedResources.splice(resIndex, 1);
   },
 };
 </script>
